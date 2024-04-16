2df9a3192c13f43dcde2014adc030c783ce849ee
ahmadyogi543 <ahmadyogi543@gmail.com>
2024-04-16 13:13:25 +0800

    fix: rely on WASM now

--- ui/src/context/PHPWebContext.jsx
+++ ui/src/context/PHPWebContext.jsx
@@ -1,6 +1,6 @@
 import React, {createContext, useContext, useState} from 'react';
 
-import usePHPWeb from '../hooks/usePHPWeb';
+import usePHPWeb from '../hooks/usePHPWASM';
 
 const PHPWebContext = createContext();
 
